3cbe9c3ea940c295e99775a577aaa62d90cd196a
joeplaydon <bodthegod@gmail.com>
2024-11-18 12:30:40 +0000

    - Update content

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -218,7 +218,7 @@ const about = {
 
 const blog = {
   label: "Blog",
-  title: "Writing about design and tech...",
+  title: "Writing about my journey in tech...",
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
